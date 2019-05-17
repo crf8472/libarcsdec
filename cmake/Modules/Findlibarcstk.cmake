a508d536e46b79a2661673c55762d5cb3982dc59
tristero <crf8472@web.de>
2019-05-17 17:39:50 +0200

    Remove WIN32 check

--- cmake/Modules/Findlibarcstk.cmake
+++ cmake/Modules/Findlibarcstk.cmake
@@ -1,7 +1,7 @@
-# libarcstk_FOUND
-# libarcstk_INCLUDE_DIR
-# libarcstk_LIBRARIES
-# libarcstk_DEFINITIONS
+## libarcstk_FOUND
+## libarcstk_INCLUDE_DIR
+## libarcstk_LIBRARIES
+## libarcstk_DEFINITIONS
 
 if (LIBARCSTK_INCLUDE_DIR AND LIBARCSTK_LIBRARY )
 
@@ -9,30 +9,38 @@ if (LIBARCSTK_INCLUDE_DIR AND LIBARCSTK_LIBRARY )
 	set(libarcstk_FIND_QUIETLY TRUE )
 endif ()
 
-if (NOT WIN32)
+## try using pkg-config to get the directories and then use these values
+## in the find_path() and find_library() calls
+## also fills in LIBARCSTK_DEFINITIONS, although that isn't normally useful
 
-	## try using pkg-config to get the directories and then use these values
-	## in the find_path() and find_library() calls
-	## also fills in LIBARCSTK_DEFINITIONS, although that isn't normally useful
-	find_package (PkgConfig QUIET )
+find_package (PkgConfig QUIET )
+
+if (PkgConfig_FOUND )
+
 	pkg_check_modules (PC_LIBARCSTK QUIET libarcstk )
+
 	set (LIBARCSTK_DEFINITIONS    ${PC_LIBARCSTK_CFLAGS_OTHER} )
 	set (LIBARCSTK_VERSION_STRING ${PC_LIBARCSTK_VERSION} )
 endif ()
 
-find_path (LIBARCSTK_INCLUDE_DIR arcstk/calculate.hpp
-	HINTS
-	${PC_LIBARCSTK_INCLUDEDIR}
-	${PC_LIBARCSTK_INCLUDE_DIRS}
+find_path (LIBARCSTK_INCLUDE_DIR
+	NAME arcstk/calculate.hpp
+	PATHS
+		${PC_LIBARCSTK_INCLUDEDIR}
+		${PC_LIBARCSTK_INCLUDE_DIRS}
 )
 
-find_library (LIBARCSTK_LIBRARY NAMES libarcstk arcstk
-	HINTS
-	${PC_LIBARCSTK_LIBDIR}
-	${PC_LIBARCSTK_LIBRARY_DIRS}
+find_library (LIBARCSTK_LIBRARY
+	NAMES libarcstk arcstk
+	PATHS
+		${PC_LIBARCSTK_LIBDIR}
+		${PC_LIBARCSTK_LIBRARY_DIRS}
 )
 
-mark_as_advanced (LIBARCSTK_INCLUDE_DIR LIBARCSTK_LIBRARY )
+mark_as_advanced (
+	LIBARCSTK_INCLUDE_DIR
+	LIBARCSTK_LIBRARY
+)
 
 include (FindPackageHandleStandardArgs )
 
