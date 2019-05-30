a49ad198ffaed8c063d876ab7c439a18ed407972
tristero <crf8472@web.de>
2019-05-30 11:28:11 +0200

    Findlibcue: Add experimental version checking in absense of pkg-config

--- cmake/Modules/Findlibcue.cmake
+++ cmake/Modules/Findlibcue.cmake
@@ -7,6 +7,11 @@
 ## libcue_INCLUDE_DIRS
 ## libcue_LIBRARIES
 
+## Libcue ships without a version file. Thus, if pkg-config is not available,
+## we cannot verify the version and have basically lost. There is a slight
+## chance that the include path contains a version number. This module therefore
+## tries to find a header and parse the version number from this header.
+
 if (LIBCUE_INCLUDE_DIR AND LIBCUE_LIBRARY )
 
 	# Already in cache, so be quiet
@@ -22,20 +27,21 @@ if (PkgConfig_FOUND )
 
 	PKG_CHECK_MODULES (PC_LIBCUE QUIET libcue )
 
-	set (LIBCUE_FOUND        ${PC_LIBCUE_FOUND} )
 	set (LIBCUE_VERSION      ${PC_LIBCUE_VERSION} )
 	set (LIBCUE_INCLUDE_DIRS ${PC_LIBCUE_INCLUDE_DIRS} )
 	set (LIBCUE_LIBRARIES    ${PC_LIBCUE_LIBRARIES} )
 
+else ()
+
+	message (WARNING "Tool pkg-config is missing, try to find libcue anyway" )
+
 endif (PkgConfig_FOUND )
 
-## 2: find includes
+## 2: find includes (and maybe version)
 
 ## If pkgconfig is not available, we try to get the version number from
 ## the include path.
 if (NOT LIBCUE_VERSION )
-
-	message (STATUS "Guess version" )
 
 	## Search for subpaths containing a version number, e.g.
 	## /usr/include/libcue-2.0/libcue/libcue.h
@@ -55,7 +61,7 @@ if (NOT LIBCUE_VERSION )
 	foreach(_path IN LISTS PATHS_WITH_VERSION )
 
 		## Get version number (hopefully)
-		string (REGEX REPLACE ".+-\([0-9\.]+\)$" "\\1" PATH_VERSION ${_path} )
+		string (REGEX REPLACE ".+-([0-9\.]+)$" "\\1" PATH_VERSION ${_path} )
 
 		## Create a list of version paths with entries formatted as
 		## 'version:path' that is easily sortable by the version prefix
@@ -81,6 +87,7 @@ if (NOT LIBCUE_VERSION )
 
 endif()
 
+
 find_path (LIBCUE_INCLUDE_DIR
 	NAMES "libcue/libcue.h"
 	PATHS
@@ -88,31 +95,46 @@ find_path (LIBCUE_INCLUDE_DIR
 	${CUSTOM_PATHS}
 )
 
-## Get actual version found, if required, but do not overwrite found version
+
+## If header is found but version is unknown, take the version from CUSTOM_PATHS
+## and search for a matching library
 if (LIBCUE_INCLUDE_DIR AND NOT LIBCUE_VERSION )
 
-	string (REGEX REPLACE ".+-\([0-9\.]+\)$" "\\1" LIBCUE_VERSION
+	string (REGEX REPLACE ".+-([0-9\.]+)$" "\\1" LIBCUE_VERSION
 		${LIBCUE_INCLUDE_DIR} )
+
+	string (REGEX REPLACE "^([0-9]+).*" "\\1" LIBCUE_VERSION_MAJOR
+		${LIBCUE_VERSION} )
+
+	## FIXME Does not seem to be best practise: with pkg-config, we will just
+	## link against libcue.so because we can rely on the pc-metadata.
+	## Without pkg-config, we could guess the version from the path, but
+	## there might be different versions installed. Therefore we try to link to
+	## a versioned .so object. This is not actually 'wrong' but it will be very
+	## surprising that cmake varies the linking instruction changes dependent
+	## on whether pkg-config is present or not.
+	## Alternative: when pkg-config is not present, just give up and explode.
+	## This will happen anyway when trying to find the other libs without
+	## pkg-config.
+
+	find_library (LIBCUE_LIBRARY
+		NAMES libcue.so.${LIBCUE_VERSION} libcue.so.${LIBCUE_VERSION_MAJOR}
+		PATHS
+		${PC_LIBCUE_LIBDIR}
+		${PC_LIBCUE_LIBRARY_DIRS}
+	)
 endif()
 
-## 3: find library
 
-## TODO In case we guessed the include dir correctly by dirname, we must
-## guarantee to find the exact corresponding lib (since we probably have several
-## versions installed)
+## 3: find library (if not yet found)
 
 find_library (LIBCUE_LIBRARY
 	NAMES cue libcue
 	PATHS
-	${PC_LIBCUE_LIBDIR}
-	${PC_LIBCUE_LIBRARY_DIRS}
+	${LIBCUE_LIBRARIES}
 )
 
-## 4: handle REQUIRED and QUIET options, set _FOUND VARIABLE
-
-if (LIBCUE_VERSION AND LIBCUE_INCLUDE_DIRS AND LIBCUE_LIBRARIES )
-	set (LIBCUE_FOUND TRUE )
-endif ()
+## 4: handle options
 
 include (FindPackageHandleStandardArgs )
 
@@ -123,15 +145,15 @@ find_package_handle_standard_args (libcue
 
 ## 5: set declared variables
 
-set (libcue_FOUND        ${LIBCUE_FOUND} )
 set (libcue_VERSION      ${LIBCUE_VERSION} )
 set (libcue_INCLUDE_DIRS ${LIBCUE_INCLUDE_DIR} )
 set (libcue_LIBRARIES    ${LIBCUE_LIBRARY} )
 
 mark_as_advanced (
-	LIBCUE_FOUND
 	LIBCUE_VERSION
 	LIBCUE_INCLUDE_DIR
+	LIBCUE_INCLUDE_DIRS
 	LIBCUE_LIBRARY
+	LIBCUE_LIBRARIES
 )
 
