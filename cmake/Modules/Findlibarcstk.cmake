3c74d22ef485577b5973b5a4fea6339c5a1e16f3
tristero <crf8472@web.de>
2019-05-30 15:22:18 +0200

    Use imported target from Findlibarcstk.cmake

--- cmake/Modules/Findlibarcstk.cmake
+++ cmake/Modules/Findlibarcstk.cmake
@@ -1,40 +1,40 @@
 ## libarcstk_FOUND
+## libarcstk_VERSION
 ## libarcstk_INCLUDE_DIR
 ## libarcstk_LIBRARIES
-## libarcstk_DEFINITIONS
 
-if (LIBARCSTK_INCLUDE_DIR AND LIBARCSTK_LIBRARY )
+if (LIBARCSTK_VERSION AND LIBARCSTK_INCLUDE_DIR AND LIBARCSTK_LIBRARY )
 
 	## already in cache
 	set(libarcstk_FIND_QUIETLY TRUE )
 endif ()
 
-## try using pkg-config to get the directories and then use these values
-## in the find_path() and find_library() calls
-## also fills in LIBARCSTK_DEFINITIONS, although that isn't normally useful
+## Try to use pkg-config to generate path hints
 
 find_package (PkgConfig QUIET )
 
 if (PkgConfig_FOUND )
 
-	pkg_check_modules (PC_LIBARCSTK QUIET libarcstk )
+	PKG_CHECK_MODULES (PC_LIBARCSTK QUIET libarcstk )
 
-	set (LIBARCSTK_DEFINITIONS    ${PC_LIBARCSTK_CFLAGS_OTHER} )
-	set (LIBARCSTK_VERSION_STRING ${PC_LIBARCSTK_VERSION} )
+	set (LIBARCSTK_VERSION ${PC_LIBARCSTK_VERSION} )
+
 endif ()
 
 find_path (LIBARCSTK_INCLUDE_DIR
-	NAME arcstk/calculate.hpp
+	NAME "arcstk/calculate.hpp"
 	PATHS
 		${PC_LIBARCSTK_INCLUDEDIR}
 		${PC_LIBARCSTK_INCLUDE_DIRS}
+	DOC "Headers of libarcstk"
 )
 
 find_library (LIBARCSTK_LIBRARY
-	NAMES libarcstk arcstk
+	NAMES "libarcstk" "arcstk"
 	PATHS
 		${PC_LIBARCSTK_LIBDIR}
 		${PC_LIBARCSTK_LIBRARY_DIRS}
+	DOC "Lib path for libarcstk"
 )
 
 mark_as_advanced (
@@ -45,12 +45,27 @@ mark_as_advanced (
 include (FindPackageHandleStandardArgs )
 
 find_package_handle_standard_args (libarcstk
-	REQUIRED_VARS LIBARCSTK_LIBRARY LIBARCSTK_INCLUDE_DIR
-	VERSION_VAR   LIBARCSTK_VERSION_STRING
+	REQUIRED_VARS LIBARCSTK_LIBRARY LIBARCSTK_VERSION LIBARCSTK_INCLUDE_DIR
+	VERSION_VAR   LIBARCSTK_VERSION
 )
 
+message (STATUS "FOUND: ${libarcstk_FOUND}" )
+
+## Export oldstyle variables for backwards compatibility
 if (libarcstk_FOUND )
-    set (libarcstk_LIBRARIES    ${LIBARCSTK_LIBRARY} )
-    set (libarcstk_INCLUDE_DIRS ${LIBARCSTK_INCLUDE_DIR} )
+    set (libarcstk_VERSION      "${LIBARCSTK_VERSION}" )
+    set (libarcstk_LIBRARIES    "${LIBARCSTK_LIBRARY}" )
+    set (libarcstk_INCLUDE_DIRS "${LIBARCSTK_INCLUDE_DIR}" )
 endif()
 
+## Export "modern" target
+## Can be used more or less as if CONFIG whould have been used instead of MODULE
+## (has not Release/Debug configuration support though)
+add_library (libarcstk::libarcstk SHARED IMPORTED )
+
+set_target_properties (libarcstk::libarcstk
+	PROPERTIES
+	INTERFACE_INCLUDE_DIRECTORIES "${libarcstk_INCLUDE_DIRS}"
+	INTERFACE_LINK_LIBRARIES      "${libarcstk_LIBRARIES}" ## TODO Necessary ?
+	IMPORTED_LOCATION             "${libarcstk_LIBRARIES}"
+)
