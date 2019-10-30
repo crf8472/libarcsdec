957b49caa22c15d0b19e8e988e0b2cb530fddf97
tristero <crf8472@web.de>
2019-10-30 17:26:17 +0100

    Add test stub for audioreader

---
+++ test/src/audioreader.cpp
@@ -0,0 +1,21 @@
+#include "catch2/catch.hpp"
+
+#ifndef __LIBARCSDEC_AUDIOREADER_HPP__
+#include "audioreader.hpp"
+#endif
+
+/**
+ * \file audioreader.cpp Tests for all API classes exported by audioreader.hpp
+ */
+
+// TODO Test ByteConverter
+
+TEST_CASE ( "List descriptors", "[byteconverter]" )
+{
+}
+
+TEST_CASE ( "Test stub (does nothing)", "[byteconverter]" )
+{
+}
+
+
