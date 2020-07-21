5b1f9629ebf4488b9a64f53b116bb81554cf1030
tristero <crf8472@web.de>
2020-07-21 23:21:14 +0200

    Update doxygen documentation

---
+++ doc/thirdparty/doxygen/dedup_index.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import lxml.etree as ET
+
+def xsl_transform(inDoc, xslFile, outDoc):
+    doc = ET.parse(inDoc)
+    xslt = ET.parse(xslFile)
+    transform = ET.XSLT(xslt)
+    transformed_doc = transform(doc)
+    with open(outDoc, 'w') as f:
+        print(transformed_doc, file=f)
+    return
+
+def doxy_deduplicate_index(filename):
+    xsl_transform(filename,
+            "@PROJECT_DOC_DIR@/thirdparty/doxygen/dedup_index.xsl", filename)
+    print('Removed duplicated <compound> elements in index.xml')
+
+doxy_deduplicate_index(sys.argv[1])
+
