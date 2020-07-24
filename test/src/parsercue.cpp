a38a5b38f6ce0df0d7539a21cec69f259beb260e
tristero <crf8472@web.de>
2020-07-24 20:47:38 +0200

    Add stub for testing parsercue

---
+++ test/src/parsercue.cpp
@@ -0,0 +1,12 @@
+#include "catch2/catch.hpp"
+
+#ifndef __LIBARCSDEC_PARSERCUE_HPP__
+#include "parsercue.hpp"
+#endif
+
+/**
+ * \file
+ *
+ * Tests for all API classes exported by parsercue.hpp
+ */
+
