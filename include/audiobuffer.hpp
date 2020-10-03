e8f83e8464045acc48ad84990c7baf017bea982e
tristero <crf8472@web.de>
2020-10-03 11:52:48 +0200

    Remove unused BlockAccumulator and BufferProcessor

--- include/audiobuffer.hpp
+++ include/audiobuffer.hpp
@@ -2,7 +2,6 @@
 #define __LIBARCSDEC_AUDIOBUFFER_HPP__
 
 /**
- * \deprecated
  * \file
  *
  * \brief Toolkit for buffering audio samples
@@ -27,39 +26,30 @@ inline namespace v_1_0_0
  * \brief Tool classes for buffering audio samples.
  *
  * Decouple the amount of bytes read at once and the amount of samples
- * processed in one step by buffering the audio samples.
+ * processed in one step by buffering the audio samples in an intermediate
+ * step.
  *
- * Note that the block size of an AudioReaderImpl set by
- * set_samples_per_block() does not define how many samples are to be read in a
- * single operation. Instead, this definition controls how many samples are to
- * be pushed to calculation at once. The block size is therefore not about
- * reading bytes at once but about how many bytes approximately are represented
- * in the memory at once. In fact, it may be more if the buffer is flushed in
- * the middle of a sequence and a fragment of the sequence is postponed to the
- * next buffer.
+ * The basic buffering interface is BlockCreator. A BlockCreater can be
+ * parametized with a number of sample that specifies the usual size of the
+ * blocks it is expected to create. The last block, however, may be smaller
+ * than this size.
  *
- * In fact, the number of decoded samples per sequence is the value that
- * identifies how many bytes are read at once. This is not configurable in the
- * current version, but specific for each AudioReaderImpl and may or may not
- * depend on the current block size. Note that decoded samples is not to be
- * identified with "32 bit PCM samples" but with whatever the decoder returns.
- * Usually, these are signed integers of 32 bit representing a single 16 bit
- * sample that may have positive or negative values.
+ * PCMBlockReader is build on this interface.
  *
- * The splitting of sequences can be entirely avoided for combinations of
- * sequence size and block size where the block size is a multiple of a
- * sequence size so the sequence are aligned to the blocks.
+ * \note Motivation: The readers based on libFLAC++ and ffmpeg do not seem to
+ * provide any capability of specifying how many samples to read at once. So
+ * each frame is passed to arcstk::Calculation in its own call of \c update().
+ * This seems inflexible and may lead to huge logfiles. It may be a requirement
+ * to control the amount of samples that used for a single \c update(). Most
+ * readers can do this by just specifying the amount of samples or bytes to read
+ * in one read operation. For the readers that do not offer this option,
+ * buffering is a solution.
  *
- * The basic buffering interface is BlockCreator.
- *
- * The most facilitated subclass of BlockCreator is BlockAccumulator that
- * implements the accumulation of sequences of decoded samples into blocks of
- * predefined size. BlockAccumulator targets situations in which reading an
- * entire block of samples in one I/O operation is not intended.
- * BlockAccumulator buffers up sequences of samples until the defined buffer
- * size is reached.
- *
- * Classes BufferProcessor and PCMBlockReader are built on this API.
+ * \warning This interface is just a stub at the moment. The ffmpeg-based reader
+ * could just use an AVAudioFifo but that would either be a solution for only
+ * one reader and make the implementation dependent on the availability of
+ * ffmpeg (3rd party dependency). The FLAC reader should not depend on anything
+ * except libFLAC and libFLAC++.
  *
  * @{
  */
@@ -156,137 +146,6 @@ private:
 	 * \brief Number of 32bit PCM samples per block
 	 */
 	int32_t samples_per_block_;
-};
-
-
-/**
- * \deprecated
- * \brief Basic buffer: specify size, fill with samples and then flush.
- *
- * Accumulates SampleSequences to the configured block size and passes the
- * block to a consumer as soon as it is filled completely.
- *
- * In come sequences of non-zero size smaller than the block, out go blocks of
- * the defined size.
- *
- * Not every decoder is compatible to pull-reading a specified block size.
- * If it is required or advantageous to read the samples in smaller sequences,
- * the sequences have to be accumulated to a block. To make a decoder
- * compatible with this strategy, define an appropriate subclass of
- * SampleSequence.
- */
-class BlockAccumulator : public virtual BlockCreator
-{
-public:
-
-	/**
-	 * \brief Default constructor.
-	 *
-	 * Constructs a BlockAccumulator with buffer of size BLOCKSIZE::DEFAULT
-	 */
-	BlockAccumulator();
-
-	/**
-	 * \brief Constructs a BlockAccumulator with buffer of size
-	 * samples_per_block.
-	 *
-	 * \param[in] samples_per_block Number of 32 bit PCM samples in one block
-	 */
-	explicit BlockAccumulator(const int32_t samples_per_block);
-
-	// make class non-copyable (1/2)
-	BlockAccumulator(const BlockAccumulator &) = delete;
-
-	/**
-	 * \brief Virtual default destructor
-	 */
-	~BlockAccumulator() noexcept override;
-
-	/**
-	 * \brief Registers a consuming method for sample sequences.
-	 *
-	 * \param[in] func The functor to be registered as sample consumer.
-	 */
-	void register_block_consumer(const std::function<void(
-			SampleInputIterator begin, SampleInputIterator end)> &func);
-
-	/**
-	 * \brief Call this method before passing the first sample sequence
-	 */
-	void init();
-
-	/**
-	 * \brief Appends a sample sequence to the buffer.
-	 *
-	 * It is guaranteed that the entire sequence is appended. However,
-	 * if the sequence is longer than the remaining buffer capacity, the buffer
-	 * is immediately flushed when full, so appending may cause flushing. Hence
-	 * it is not guaranteed, that the entire sequence is part of the same block.
-	 *
-	 * \param[in] begin Begin of the sample sequence
-	 * \param[in] end   End of the sample sequence
-	 */
-	void append_to_block(SampleInputIterator begin, SampleInputIterator end);
-
-	/**
-	 * \brief Call this method after having passed the last sample sequence
-	 */
-	void flush();
-
-	/**
-	 * \brief Returns the number of samples processed
-	 *
-	 * \return Number of samples processed since init() was called
-	 */
-	int32_t samples_appended() const;
-
-	// make class non-copyable (2/2)
-	BlockAccumulator& operator = (const BlockAccumulator &) = delete;
-
-protected:
-
-	/**
-	 * \brief Reinitialize internal buffer to specified size as number of 32 bit
-	 * PCM samples.
-	 *
-	 * \param[in] total_samples Reinitialize buffer for new block
-	 */
-	void init_buffer(const int32_t total_samples);
-
-private:
-
-	/**
-	 * \brief Implementation of init()
-	 */
-	virtual void do_init();
-
-	/**
-	 * \brief Implementation of flush()
-	 */
-	virtual void do_flush();
-
-	/**
-	 * \brief Reinitialize internal buffer to configured block size.
-	 */
-	virtual void init_buffer();
-
-	/**
-	 * \brief Registered callback method to consume a block.
-	 *
-	 * Called by block_complete().
-	 */
-	std::function<void(SampleInputIterator begin, SampleInputIterator end)>
-		consume_;
-
-	/**
-	 * \brief Internal sample buffer
-	 */
-	std::vector<uint32_t> samples_; // TODO Use arcstk::sample_t
-
-	/**
-	 * \brief Number of samples processed
-	 */
-	int32_t samples_appended_;
 };
 
 /// @}
