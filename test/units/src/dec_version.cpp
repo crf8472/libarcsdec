0a64b99bba985a32d72a1de92db2b21eeb5e1f3a
tristero <crf8472@mailbox.org>
2026-06-22 09:45:48 +0200

    Set version to 0.3.0

--- test/units/src/dec_version.cpp
+++ test/units/src/dec_version.cpp
@@ -16,10 +16,10 @@ TEST_CASE ( "constants", "[version]" )
 	CHECK ( arcsdec::v_1_0_0::LIBARCSDEC_NAME == "libarcsdec" );
 
 	CHECK ( arcsdec::v_1_0_0::LIBARCSDEC_VERSION_MAJOR  == 0 );
-	CHECK ( arcsdec::v_1_0_0::LIBARCSDEC_VERSION_MINOR  == 2 );
+	CHECK ( arcsdec::v_1_0_0::LIBARCSDEC_VERSION_MINOR  == 3 );
 	CHECK ( arcsdec::v_1_0_0::LIBARCSDEC_VERSION_PATCH  == 0 );
-	CHECK ( arcsdec::v_1_0_0::LIBARCSDEC_VERSION_SUFFIX == "alpha.3" );
-	CHECK ( arcsdec::v_1_0_0::LIBARCSDEC_VERSION        == "0.2.0-alpha.3" );
+	CHECK ( arcsdec::v_1_0_0::LIBARCSDEC_VERSION_SUFFIX == "alpha.1" );
+	CHECK ( arcsdec::v_1_0_0::LIBARCSDEC_VERSION        == "0.3.0-alpha.1" );
 }
 
 
@@ -34,8 +34,9 @@ TEST_CASE ( "functions", "[version]" )
 		CHECK (  api_version_is_at_least(0, 0, 2) );
 		CHECK (  api_version_is_at_least(0, 1, 0) );
 		CHECK (  api_version_is_at_least(0, 1, 1) );
-		CHECK (  api_version_is_at_least(0, 2, 0) ); // <= this version
-		CHECK ( !api_version_is_at_least(0, 2, 1) );
+		CHECK (  api_version_is_at_least(0, 2, 0) );
+		CHECK (  api_version_is_at_least(0, 2, 5) );
+		CHECK (  api_version_is_at_least(0, 3, 0) ); // <= this version
 		CHECK ( !api_version_is_at_least(1, 0, 0) );
 		CHECK ( !api_version_is_at_least(1, 1, 0) );
 		CHECK ( !api_version_is_at_least(1, 1, 1) );
