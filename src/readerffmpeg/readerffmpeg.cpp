cfeddb61dd4d51b1f63af74fba756dbb72e4ad23
tristero <crf8472@mailbox.org>
2026-06-29 20:53:10 +0200

    Fix: use av_frame_free on frames

--- src/readerffmpeg/readerffmpeg.cpp
+++ src/readerffmpeg/readerffmpeg.cpp
@@ -278,7 +278,8 @@ AVPacketPtr Make_AVPacketPtr::operator()() const
 
 void Free_AVFrame::operator()(::AVFrame* frame) const
 {
-	::av_free(frame);
+	//::av_free(frame);
+	::av_frame_free(&frame);
 	frame = nullptr;
 }
 
