ca55ba88e6dccc6d430c913840eb46865e9ecdc6
tristero <crf8472@mailbox.org>
2026-07-07 19:41:45 +0200

    Fix inclusion of bison header

--- src/features/parsercue/src/cuesheet/cuesheet_lexer_defs.hpp
+++ src/features/parsercue/src/cuesheet/cuesheet_lexer_defs.hpp
@@ -30,7 +30,7 @@
 // Include the Token definitions as well as the redefined yylex()
 // in section "code top" (that calls get_next_token())
 #ifndef LIBARCSDEC_CUESHEET_TAB_HPP_
-#include "cuesheet.tab.hpp"
+#include "cuesheet_tab.hpp"
 #endif
 
 #if defined(__clang__)
